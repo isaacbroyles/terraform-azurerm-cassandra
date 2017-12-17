0bff0471e8a0af1705f6732d75e48f4d67c7a10d
Isaac Broyles <isaacbroyles@gmail.com>
2017-12-16 18:01:38 -0600

    Remove unnecessary targets

--- terraform-common.mk
+++ terraform-common.mk
@@ -4,10 +4,6 @@ INFRA ?= $(word 1,$(subst -, ,$(ENV_NAME)))
 ENV_TAIL ?= $(subst $(INFRA)-,,$(ENV_NAME))
 TFVARS := $(PWD)/terraform.tfvars
 TFSTATE := $(PWD)/.terraform/terraform.tfstate
-TRAVIS_BUILD_COM_HOST ?= build.travis-ci.com
-TRAVIS_BUILD_ORG_HOST ?= build.travis-ci.org
-JOB_BOARD_HOST ?= job-board.travis-ci.com
-AMQP_URL_VARNAME ?= AMQP_URL
 TOP := $(shell git rev-parse --show-toplevel)
 
 PROD_TF_VERSION := v0.11.1
@@ -71,30 +67,9 @@ clean: announce
 distclean: clean
 	$(RM) -r .terraform/
 
-.PHONY: graph
-graph:
-	$(TERRAFORM) graph -draw-cycles | dot -Tpng > graph.png
-
 $(ENV_NAME).tfvars:
 	$(TOP)/bin/generate-tfvars $@
 
 .PHONY: list
 list:
 	 @$(MAKE) -pRrq -f $(lastword $(MAKEFILE_LIST)) : 2>/dev/null | awk -v RS= -F: '/^# File/,/^# Finished Make data base/ {if ($$1 !~ "^[#.]") {print $$1}}' | sort | egrep -v -e '^[^[:alnum:]]' -e '^$@$$' | xargs
-
-.PHONY: check
-check:
-	$(TOP)/bin/pre-flight-checks $@
-
-config/.written:
-	$(TOP)/bin/write-config-files \
-		--infra "$(INFRA)" \
-		--env "$(ENV_SHORT)" \
-		--build-com-host "$(TRAVIS_BUILD_COM_HOST)" \
-		--build-org-host "$(TRAVIS_BUILD_ORG_HOST)" \
-		--job-board-host "$(JOB_BOARD_HOST)" \
-		--amqp-url-varname "$(AMQP_URL_VARNAME)" $(WRITE_CONFIG_OPTS)
-
-config/.gce-keys-written:
-	cp -v $$TRAVIS_KEYCHAIN_DIR/travis-keychain/gce/*.json config/
-	date -u >$@
